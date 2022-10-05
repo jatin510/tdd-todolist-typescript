dda21e5e162ffbcd38ae8a6cc21cd4f989393736
jatin510 <jatin6972@gmail.com>
2022-10-05 22:37:27 +0530

    added ability to create a empty todoList

--- src/modules/todo/domain/todoList.spec.ts
+++ src/modules/todo/domain/todoList.spec.ts
@@ -1,26 +1,10 @@
-import TodoList from './todoList';
+import TodoList, { ITodoListProps } from './todoList';
 
-describe('Testing TODO Domain', () => {
-  it('Todo should have an id', () => {
-    const todoListProps = {};
-    const todoList = new TodoList(todoListProps);
-    expect(typeof todoList.id === 'string').toBeTruthy();
-  });
-
-  it('We should be able to assign id', () => {
-    const todoListProps = {
-      id: 'ABC',
-    };
-    const todoList = new TodoList(todoListProps);
-    expect(todoList.id).toEqual(todoListProps.id);
-  });
-
-  it("If we don't assign id to task, it should generate random non empty string id", () => {
-    const todoListProps = {};
-    const todoList1 = new TodoList(todoListProps);
-    expect(todoList1.id.length !== 0).toBeTruthy();
-
-    const todoList2 = new TodoList(todoListProps);
-    expect(todoList1.id !== todoList2.id).toBeTruthy();
+describe('Testing TodoList', () => {
+  test('should create a list of empty todos', () => {
+    const todoListProps: ITodoListProps = {};
+    const todoList = TodoList.create(todoListProps);
+    expect(todoList).toBeDefined();
+    expect(todoList.todos).toEqual([]);
   });
 });
--- src/modules/todo/domain/todoList.ts
+++ src/modules/todo/domain/todoList.ts
@@ -1,19 +1,24 @@
-import { randomUUID } from 'crypto';
 import { Entity } from '../../../shared/domain/entity';
 import { Todo } from './todo';
 
-// export type TodoListProps = {
-//   id?: string;
-//   todos: Todo[];
-// };
+export interface ITodoListProps {
+  todos?: Todo[];
+}
 
-export default class TodoList extends Entity<null> {
-  todos: Todo[];
-
-  constructor() {
-    super(null);
-    this.todos = [];
+export default class TodoList extends Entity<ITodoListProps> {
+  private constructor(props: ITodoListProps) {
+    super(props);
   }
 
-  public static;
+  get todos() {
+    return this.props.todos;
+  }
+
+  public static create(props: ITodoListProps): TodoList {
+    const todoList = new TodoList({
+      ...props,
+      todos: props.todos ? props.todos : [],
+    });
+    return todoList;
+  }
 }
