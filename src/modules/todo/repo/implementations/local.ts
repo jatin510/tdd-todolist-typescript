9e03fa28868169e29708aabeba3734fb71213635
jatin510 <jatin6972@gmail.com>
2022-10-14 00:06:24 +0530

    added functions in todo repo

--- src/modules/todo/repo/implementations/local.ts
+++ src/modules/todo/repo/implementations/local.ts
@@ -1,5 +1,5 @@
-import { ITodoRepo } from "..";
-import { Todo } from "../../domain/todo";
+import { ITodoRepo } from '..';
+import { Todo } from '../../domain/todo';
 
 class LocalTodoRepo implements ITodoRepo {
   private _todos = [];
@@ -8,6 +8,17 @@ class LocalTodoRepo implements ITodoRepo {
 
   get todos() {
     return this._todos;
+  }
+
+  async update(todo: Partial<Todo>): Promise<void> {
+    const olderTodo = this._todos.filter((todo) => todo.id === todo.id);
+    const updatedTodo = { ...olderTodo, ...todo };
+    this._todos.push(updatedTodo);
+  }
+
+  async findById(id: string): Promise<Todo> {
+    const todo = this._todos.find((todo) => todo.id === id);
+    return todo;
   }
 
   async save(todo: Todo): Promise<void> {
--- src/modules/todo/repo/index.ts
+++ src/modules/todo/repo/index.ts
@@ -1,5 +1,7 @@
-import { Todo } from "../domain/todo";
+import { Todo } from '../domain/todo';
 
 export interface ITodoRepo {
+  findById(id: string): Promise<Todo>;
+  update(todo: Partial<Todo>): Promise<void>;
   save(todo: Todo): Promise<void>;
 }
